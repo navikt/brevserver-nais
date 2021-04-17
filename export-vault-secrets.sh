67acfe656d61d5170d93075d2b9c9619273c6b00
S161464 <hakon.snotun@nav.no>
2021-04-17 19:34:19 +0200

    MMA-5199: Tester config

--- export-vault-secrets.sh
+++ export-vault-secrets.sh
@@ -3,14 +3,14 @@
 if test -f /var/run/secrets/nais.io/brevserverDS/username;
 then
     echo "Setting BREVSERVER_DS_USERNAME"
-    export  BREVSERVER_DS_USERNAME=$(cat /var/run/secrets/nais.io/brevserversDS/username)
+    export  BREVSERVER_DS_USERNAME=$(cat /var/run/secrets/nais.io/brevserverDS/username)
 fi
 if test -f /var/run/secrets/nais.io/brevserverDS/password;
 then
     echo "Setting BREVSERVER_DS_PASSWORD"
     export  BREVSERVER_DS_PASSWORD=$(cat /var/run/secrets/nais.io/brevserverDS/password)
 fi
-if test -f /var/run/secrets/nais.io/nrevserverDS/url;
+if test -f /var/run/secrets/nais.io/brevserverDS/url;
 then
     echo "Setting BREVSERVER_DS_URL"
     export  BREVSERVER_DS_URL=$(cat /var/run/secrets/nais.io/brevserverDS/url)
