9a73350c797defe51b0a1b36c3bea24f9c8ac4a3
S161464 <hakon.snotun@nav.no>
2021-06-17 09:30:37 +0200

    MMA-5200: La til DefaultMapper

--- brevserver-app/src/main/java/no/nav/brevserver/nais/support/AvbrytDokumentRequestMapper.java
+++ brevserver-app/src/main/java/no/nav/brevserver/nais/support/AvbrytDokumentRequestMapper.java
@@ -3,7 +3,7 @@ package no.nav.brevserver.nais.support;
 import no.nav.brevserver.server.common.to.AvbrytDokumentRequest;
 import org.springframework.stereotype.Component;
 
-@Component
+
 public interface AvbrytDokumentRequestMapper {
 	/**
 	 * Maps from webservice request to domain request for avbrytDokument
