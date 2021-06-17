2dce044139e9e598ba6bd7e3f52b97224d47a00a
S161464 <hakon.snotun@nav.no>
2021-06-17 10:51:51 +0200

    MMA-5200: Cleanup

--- export-vault-secrets.sh
+++ export-vault-secrets.sh
@@ -1,12 +1,1 @@
 #!/usr/bin/env sh
-
-if test -f /var/run/secrets/nais.io/vault/brevserver_ds_username;
-then
-    echo "Setting BREVSERVER_DS_USERNAME"
-    export  BREVSERVER_DS_USERNAME=$(cat /var/run/secrets/nais.io/vault/brevserver_ds_username)
-fi
-if test -f /var/run/secrets/nais.io/vault/brevserver_ds_password;
-then
-    echo "Setting BREVSERVER_DS_PASSWORD"
-    export  BREVSERVER_DS_PASSWORD=$(cat /var/run/secrets/nais.io/vault/brevserver_ds_password)
-fi
