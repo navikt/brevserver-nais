c6e6f5e1049b8af0b2ce3d6a871fffbef4d204a2
S161464 <hakon.snotun@nav.no>
2021-06-17 10:16:53 +0200

    MMA-5200: Oppdaterte Naiserator

--- export-vault-secrets.sh
+++ export-vault-secrets.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env sh
 
-if test -f /var/run/secrets/nais.io/brevserverDS/username;
+if test -f /var/run/secrets/nais.io/vault/brevserver_ds_username;
 then
     echo "Setting BREVSERVER_DS_USERNAME"
-    export  BREVSERVER_DS_USERNAME=$(cat /var/run/secrets/nais.io/brevserverDS/username)
+    export  BREVSERVER_DS_USERNAME=$(cat /var/run/secrets/nais.io/vault/brevserver_ds_username)
 fi
-if test -f /var/run/secrets/nais.io/brevserverDS/password;
+if test -f /var/run/secrets/nais.io/vault/brevserver_ds_password;
 then
     echo "Setting BREVSERVER_DS_PASSWORD"
-    export  BREVSERVER_DS_PASSWORD=$(cat /var/run/secrets/nais.io/brevserverDS/password)
+    export  BREVSERVER_DS_PASSWORD=$(cat /var/run/secrets/nais.io/vault/brevserver_ds_password)
 fi
-if test -f /var/run/secrets/nais.io/brevserverDS/url;
+if test -f /var/run/secrets/nais.io/vault/brevserver_ds_url;
 then
     echo "Setting BREVSERVER_DS_URL"
-    export  BREVSERVER_DS_URL=$(cat /var/run/secrets/nais.io/brevserverDS/url)
+    export  BREVSERVER_DS_URL=$(cat /var/run/secrets/nais.io/vault/brevserver_ds_url)
 fi
