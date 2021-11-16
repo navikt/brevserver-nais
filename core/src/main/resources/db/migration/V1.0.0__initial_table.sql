eb2ef253b9b0cf224b36c44ae39ece28a55bdd7a
S161464 <hakon.snotun@nav.no>
2021-11-16 11:03:17 +0100

    MMA-5707: Fix Databaseconfig for Char

--- core/src/main/resources/db/migration/V1.0.0__initial_table.sql
+++ core/src/main/resources/db/migration/V1.0.0__initial_table.sql
@@ -1,75 +1,75 @@
 create table t_brevstatus
 (
-    brevreferanse     varchar2(32) not null,
-    returkoe          varchar2(100) not null,
-    bestillerbrukerid varchar2(18),
-    brevmal           varchar2(255) not null,
-    systemid          varchar2(4) not null,
-    status            varchar2(8) not null,
-    format            varchar2(32) not null,
-    skrivertype       varchar2(16),
-    skriver           varchar2(32),
-    arkiver           varchar2(3),
-    skuff             varchar2(32),
+    brevreferanse     varchar2(32 CHAR) not null,
+    returkoe          varchar2(100 CHAR) not null,
+    bestillerbrukerid varchar2(18 CHAR),
+    brevmal           varchar2(255 CHAR) not null,
+    systemid          varchar2(4 CHAR) not null,
+    status            varchar2(8 CHAR) not null,
+    format            varchar2(32 CHAR) not null,
+    skrivertype       varchar2(16 CHAR),
+    skriver           varchar2(32 CHAR),
+    arkiver           varchar2(3 CHAR),
+    skuff             varchar2(32 CHAR),
     knappstatus       integer,
     dato_endret date not null,
-    endret_av varchar2(100) not null,
+    endret_av varchar2(100 CHAR) not null,
     dato_opprettet date not null,
-    opprettet_av varchar2(100) not null
+    opprettet_av varchar2(100 CHAR) not null
 );
 
 create table t_brevsystilgang
 (
-    systemid      varchar2(4) not null,
-    systempassord varchar2(32) not null,
-    eldok_id      varchar2(20),
-    eldok_pwd     varchar2(20)
+    systemid      varchar2(4 CHAR) not null,
+    systempassord varchar2(32 CHAR) not null,
+    eldok_id      varchar2(20 CHAR),
+    eldok_pwd     varchar2(20 CHAR)
 );
 
 create table t_brevtilgang
 (
     id        number(19,0) PRIMARY KEY,
-    brevreferanse   varchar2(32),
-    token     varchar2(64),
-    systemid  varchar2(4),
+    brevreferanse   varchar2(32 CHAR),
+    token     varchar2(64 CHAR),
+    systemid  varchar2(4 CHAR),
     dato_endret date not null,
-    endret_av varchar2(100) not null,
+    endret_av varchar2(100 CHAR) not null,
     dato_opprettet date not null,
-    opprettet_av varchar2(100) not null
+    opprettet_av varchar2(100 CHAR) not null
 );
 
 create table t_brevlager
 (
     id        NUMBER(19,0) PRIMARY KEY,
-    brevreferanse varchar2(32) not null,
-    status        varchar2(8) not null,
-    brukerid      varchar2(18),
-    systemid      varchar2(4) not null,
+    brevreferanse varchar2(32 CHAR) not null,
+    status        varchar2(8 CHAR) not null,
+    brukerid      varchar2(18 CHAR),
+    systemid      varchar2(4 CHAR) not null,
     brevdata      blob      not null,
-    contenttype   varchar2(64),
+    contenttype   varchar2(64 CHAR),
     vasket        char,
     partkey       smallint,
     dato_endret date not null,
-    endret_av varchar2(100) not null,
+    endret_av varchar2(100 CHAR) not null,
     dato_opprettet date not null,
-    opprettet_av varchar2(100) not null
+    opprettet_av varchar2(100 CHAR) not null
 );
 
 create sequence t_brevlager_historikk_seq start with 1 increment by 1;
 create table t_brevlager_historikk
 (
     brevlager_historik_id integer   not null,
-    brevreferanse         varchar2(32) not null,
-    status                varchar2(8) not null,
-    brukerid              varchar2(18),
-    systemid              varchar2(4) not null,
+    brevreferanse         varchar2(32 CHAR) not null,
+    status                varchar2(8 CHAR) not null,
+    brukerid              varchar2(18 CHAR),
+    systemid              varchar2(4 CHAR) not null,
     brevdata              blob      not null,
-    contenttype           varchar2(64),
+    contenttype           varchar2(64 CHAR),
     vasket                char      not null,
     partkey               smallint,
     dato_endret date not null,
-    endret_av varchar2(100) not null,
+    endret_av varchar2(100 CHAR) not null,
     dato_opprettet date not null,
-    opprettet_av varchar2(100) not null,
+    opprettet_av varchar2(100 CHAR) not null
 );
 
