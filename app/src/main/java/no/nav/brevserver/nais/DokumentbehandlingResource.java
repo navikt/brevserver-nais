9e2de4c6b90ef4ae16f7147e49044ed529bbef08
S161464 <hakon.snotun@nav.no>
2022-07-13 10:25:39 +0200

    MMA-6245 Legge tilbake sikkerhet

--- app/src/main/java/no/nav/brevserver/nais/DokumentbehandlingResource.java
+++ app/src/main/java/no/nav/brevserver/nais/DokumentbehandlingResource.java
@@ -8,7 +8,7 @@ import no.nav.brevserver.core.exception.BrevFunctionalException;
 import no.nav.brevserver.core.exception.BrevSecurityException;
 import no.nav.brevserver.core.exception.BrevTechnicalException;
 import no.nav.brevserver.nais.swagger.SwaggerLagreBrev;
-import no.nav.security.token.support.core.api.Unprotected;
+import no.nav.security.token.support.core.api.Protected;
 import no.nav.tjenester.brevogarkiv.dokumentbehandling.AvbrytDokumentRequest;
 import no.nav.tjenester.brevogarkiv.dokumentbehandling.FerdigstillDokumentRequest;
 import no.nav.tjenester.brevogarkiv.dokumentbehandling.HentDokumentRequest;
@@ -34,7 +34,7 @@ import java.io.InputStream;
 @RequestMapping("rest")
 @RestController
 @Slf4j
-@Unprotected
+@Protected
 public class DokumentbehandlingResource {
 
 	private static final String EXCEPTION_MESSAGE = "Rest-Kall feilet";
