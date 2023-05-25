e9aa66307e77d4a18927bb0368d8a2824c919f84
Joakim Bjørnstad <joakim.bjornstad@nav.no>
2023-05-25 09:05:40 +0200

    MMA-6833 Nytt navn til JSP/Applet controlleren
    * La til warn logging

---
+++ app/src/test/java/no/nav/brevserver/web/BrevserverLegacyControllerTest.java
@@ -0,0 +1,30 @@
+package no.nav.brevserver.web;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.ui.Model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+
+@Disabled
+public class BrevserverLegacyControllerTest {
+
+	private final String expected = "brevserver";
+
+	@Test
+	public void testValidationOkPE() {
+		Model modelMock = mock(Model.class);
+		BrevserverLegacyController brevserverLegacyController = new BrevserverLegacyController("https://itest");
+		String result = brevserverLegacyController.startBrevklient("PE2", "123456789", "TOKEN", "60", "60", modelMock);
+		assertEquals(result, expected);
+	}
+
+	@Test
+	public void testValidationOkBI() {
+		Model modelMock = mock(Model.class);
+		BrevserverLegacyController brevserverLegacyController = new BrevserverLegacyController("https://itest");
+		String result = brevserverLegacyController.startBrevklient("BI12", "123456789", "TOKEN", "60", "60", modelMock);
+		assertEquals(result, expected);
+	}
+}
